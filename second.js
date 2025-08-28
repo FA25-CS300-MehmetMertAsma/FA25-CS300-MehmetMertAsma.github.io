8fa2527c798b8cafd5708324e40a11b17456af3f
mehmetmertasma <asmamehmet@myocu.oak.edu>
2025-08-28 14:26:31 -0500

    .

--- second.js
+++ second.js
@@ -1,15 +1,15 @@
 async function fetchUsers() {
-    const response = await fetch('https://fa25-cs300-mehmetmertasma.github.io/open_data.json');
+    const response = await fetch('https://fa25-cs300-mehmetmertasma.github.io/data.json');
     const users = await response.json();
     console.log("First 3 users:", users.slice(0, 3));
 }
 async function fetchUsers() {
-    const response = await fetch('https://fa25-cs300-mehmetmertasma.github.io/open_data.json');
+    const response = await fetch('https://fa25-cs300-mehmetmertasma.github.io/Books.json');
     const users = await response.json();
     console.log("First 3 users:", users.slice(0, 3));
 }
 async function fetchUsers() {
-    const response = await fetch('https://fa25-cs300-mehmetmertasma.github.io/open_data.json');
+    const response = await fetch('https://fa25-cs300-mehmetmertasma.github.io/movies.json');
     const users = await response.json();
     console.log("First 3 users:", users.slice(0, 3));
 }
